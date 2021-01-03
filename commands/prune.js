95d2724f50a0c4f62137fbf48cb98861b334d078
irfanmmr <irfanmaulanamr@student.upi.edu>
2021-01-03 14:38:15 +0700

    prune

--- commands/prune.js
+++ commands/prune.js
@@ -1,18 +1,23 @@
 module.exports = {
     name: 'prune',
     description: 'Prune up to 99 messages.',
-    execute(message, args) {
-        const amount = parseInt(args[0]) + 1;
-
-        if (isNaN(amount)) {
-            return message.reply('that doesn\'t seem to be a valid number.');
-        } else if (amount <= 1 || amount > 100) {
-            return message.reply('you need to input a number between 1 and 99.');
+    async execute(message) {
+        const args = message.content.split(' ');
+        let deleteCount = 0;
+        try {
+            deleteCount = parseInt(args[1], 10);
+        }catch(err) {
+            return message.reply('Please provide the number of messages to delete. (max 100)')
         }
 
-        message.channel.bulkDelete(amount, true).catch(err => {
-            console.error(err);
-            message.channel.send('there was an error trying to prune messages in this channel!');
+
+        if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+            return message.reply('Please provide a number between 2 and 100 for the number of messages to delete');
+
+        const fetched = await message.channel.messages.fetch({
+            limit: deleteCount,
         });
+        message.channel.bulkDelete(fetched)
+            .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
     },
 };
